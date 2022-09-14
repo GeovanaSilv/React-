d0fdac3a8b4b0969497c02fea36ec8cbf0ffbd00
GEOVANA OLIVEIRA BARROS DA SILVA <u21430@LAPA17.academico.cotuca.unicamp.br>
2022-09-14 16:45:23 -0300

    projeto escola

--- ProjetoEscola_API/Controllers/AlunoController.cs
+++ ProjetoEscola_API/Controllers/AlunoController.cs
@@ -1,3 +1,7 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using ProjetoEscola_API.Data;
 using ProjetoEscola_API.Models;
@@ -6,9 +10,10 @@ namespace ProjetoEscola_API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController] 
-    public class AlunoController : ControllerBase
+    public class AlunoController : Controller
     {
-        private EscolaContext _context;
+        private readonly EscolaContext _context;
+      
         public AlunoController(EscolaContext context){
             _context =context;
 
@@ -17,5 +22,76 @@ namespace ProjetoEscola_API.Controllers
         public ActionResult<List<Aluno>> GetAll(){
             return _context.Aluno.ToList();
         }
+        [HttpGet("{AlunoId}")]
+        public ActionResult<List<Aluno>> Get (int AlunoId){
+            try{
+                var result = _context.Aluno.Find(AlunoId);
+                if(result ==null){
+                    return NotFound();
+                }
+                return Ok(result);
+            }
+            catch{
+                return this.StatusCode(StatusCodes.Status500InternalServerError,"Falha no acesso ao banco de dados");
+            }
+        }
+       [HttpPost]
+       public async  Task<ActionResult> post(Aluno model){
+         try{
+            _context.Aluno.Add(model);
+            if(await _context.SaveChangesAsync()==1)
+          {
+             //return Ok();
+              return Created($"/api/aluno/{model.ra}",model);
+              
+         } 
+     }
+    catch{
+        return this.StatusCode(StatusCodes.Status500InternalServerError,"Falha no acesso ao banco de dados");
     }
+    return BadRequest();  
+       }
+
+
+
+       [HttpPut("{AlunoId}")]
+       public async Task <ActionResult> put (int  AlunoId, Aluno dadosAlunoAlt)
+       {
+        try{
+            var result = await _context.Aluno.FindAsync(AlunoId);
+            if(AlunoId != result.id)
+            {
+                 return BadRequest();
+            }
+            result.ra = dadosAlunoAlt.ra;
+            result.nome = dadosAlunoAlt.nome;
+            result.codCurso = dadosAlunoAlt.codCurso;
+            await _context.SaveChangesAsync();
+            return Created($"api/aluno/{dadosAlunoAlt.ra}",dadosAlunoAlt);
+        }
+        catch{
+          return this.StatusCode(StatusCodes.Status500InternalServerError,"Falha no acesso ao banco de dados.");
+        }
+
+        return Ok();
+       }
+
+
+       [HttpDelete("{AlunoRa}")]
+       public async Task <ActionResult> delete(string AlunoId){
+        try{
+            var aluno = await _context.Aluno.FindAsync(AlunoId);
+            if(aluno==null){
+                return NotFound();
+            }
+            _context.Remove(aluno);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+        catch{
+            return this.StatusCode(StatusCodes.Status500InternalServerError,"Falha no acesso ao banco de dados");
+        }
+       }    
+    }
+
 }
--- src/components/CrudAluno/CrudAluno.js
+++ src/components/CrudAluno/CrudAluno.js
@@ -1,18 +1,106 @@
 import React, {Component} from "react";
+import axios from 'axios';
 import './CrudAluno.css';
 import Main from '../template/Main';
 
 const title ="Cadastro de Alunos";
 
-const Alunos =[
-    {'id':1,'ra':1111,'nome':'André','codCurso':19},
-    {'id':2,'ra':2222,'nome':'Amanda','codCurso':28},
-    {'id':3,'ra':3333,'nome':'Pedro','codCurso':39},
-    {'id':4,'ra':4444,'nome':'Alice','codCurso':59},
+const urlAPI = "http://localhost:5278/api/Aluno";
+const initialState ={
+    aluno: {id:0,ra:'',nome:'',codCurso:0},
+    lista:[]
+}
 
-];
 
 export default class CrudAluno extends Component{
+   state = {...initialState}
+   componentDidMount(){
+    axios(urlAPI).then(resp =>{
+        this.setState({lista: resp.data})
+    })
+   }
+   limpar(){
+    this.setState({aluno : initialState.aluno});
+   }
+   salvar(){
+    const aluno = this.state.aluno;
+    aluno.codCurso = Number(aluno.codCurso);
+    const metodo = 'post';
+  
+    axios[metodo](urlAPI,aluno)
+    .then(resp =>{
+        const lista = this.getListaAtualizada(resp.data)
+        this.setState({aluno:initialState.aluno,lista})
+    })     
+   }
+   getListaAtualizada(aluno){
+    const lista = this.state.filter(a => a.id !== aluno.id);
+    lista.unshift(aluno);
+    return lista;
+   }
+   atualizaCampo(event){
+    const aluno  = {...this.state.aluno};
+
+    aluno[event.target.name]= event.target.value;
+
+    this.setState({aluno})
+   }
+    carregar(aluno){
+        const url = urlAPI +"/"+aluno.id
+        if(window.confirm("Confirma remoção do aluno" + aluno.ra)){
+            axios['delete'](url,aluno)
+            .then(resp=>{
+                const lista = this.getListaAtualizada(aluno,false)
+                this.setState({aluno: initialState.aluno,lista})
+            })
+        } 
+    }
+
+    renderForm(){
+        return(
+            <div  className="inclui-container">
+              <label> RA: </label>
+              <input
+               type ="text"
+               id="ra"
+                placeholder ="RA do aluno"
+                className ="form-input"
+                name = "ra"
+                value={this.state.aluno.ra}
+                onChange={e =>this.atualizaCampo(e)}              
+                />
+
+               <label>Nome:</label>
+               <input
+                type="text"
+                id = "nome"
+                placeholder="Nome do aluno"
+                className="form-input"
+                name="nome" 
+                value={this.state.aluno.nome}
+                onChange={e => this.atualizaCampo(e)}
+               />
+               <label>Código do Curso</label>
+               <input
+                 type= "number"
+                 id="codCurso"
+                 placeholder="0"
+                 className="form-input"
+                 name="codCurso"
+                 value={this.state.aluno.codCurso}
+                 onChange={e => this.atualizaCampo(e)}
+                 />
+                <button className="btnSalvar"
+                onClick={e => this.salvar(e)}
+                >Salvar 
+                </button>
+                <button className="btnCancelar"
+                onClick={e => this.limpar(e)}>
+                    Cancelar
+                </button>
+                 </div>
+        )
+    }
     renderTable(){
         return(
    <div className="listagem">
@@ -25,7 +113,7 @@ export default class CrudAluno extends Component{
                </tr>
            </thead>
            <tbody>
-             {Alunos.map(
+             {this.state.lista.map(
                  (aluno) =>
                  <tr key={aluno.id}>
                      <td>{aluno.ra}</td>
@@ -41,6 +129,7 @@ export default class CrudAluno extends Component{
     render(){
         return(
         <Main title={title}>
+            {this.renderForm()}
            {this.renderTable()}
             </Main>
         )
